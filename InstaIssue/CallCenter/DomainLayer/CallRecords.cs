d16cdb3565d6d6cb8c45e2c1f8c661532a93b513
Gustaf Delport <gustafdelport@gmail.com>
2021-04-20 09:58:01 +0200

    further designed the skeleton of the call center
    Still need to create setter

--- InstaIssue/CallCenter/DomainLayer/CallRecords.cs
+++ InstaIssue/CallCenter/DomainLayer/CallRecords.cs
@@ -8,5 +8,29 @@ namespace InstaIssue.CallCenter.DomainLayer
 {
     class CallRecords
     {
+        private String callRecordID;
+        private String clientID;
+        private DateTime startTimeStamp;
+        private DateTime endTimeStamp;
+        private String staffID;
+
+        public CallRecords()
+        {
+        }
+
+        public CallRecords(string callRecordID, string clientID, DateTime startTimeStamp, DateTime endTimeStamp, string staffID)
+        {
+            this.callRecordID = callRecordID;
+            this.clientID = clientID;
+            this.startTimeStamp = startTimeStamp;
+            this.endTimeStamp = endTimeStamp;
+            this.staffID = staffID;
+        }
+
+        public string CallRecordID { get => callRecordID; set => callRecordID = value; }
+        public string ClientID { get => clientID; set => clientID = value; }
+        public DateTime StartTimeStamp { get => startTimeStamp; set => startTimeStamp = value; }
+        public DateTime EndTimeStamp { get => endTimeStamp; set => endTimeStamp = value; }
+        public string StaffID { get => staffID; set => staffID = value; }
     }
 }
