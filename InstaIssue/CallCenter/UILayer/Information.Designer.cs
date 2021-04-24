9bcd6f323a952c6f67ef0407506e367cfe1e0fc2
Gustaf Delport <gustafdelport@gmail.com>
2021-04-24 11:11:23 +0200

    Completed Information Form

--- InstaIssue/CallCenter/UILayer/Information.Designer.cs
+++ InstaIssue/CallCenter/UILayer/Information.Designer.cs
@@ -32,15 +32,9 @@ namespace InstaIssue.CallCenter.UILayer
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Information));
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnDashboard = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
             this.imgLogo = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pnlInformation = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.lblDate = new System.Windows.Forms.Label();
-            this.lblTime = new System.Windows.Forms.Label();
             this.pnlSeperator = new System.Windows.Forms.Panel();
             this.lblContract = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
@@ -48,23 +42,19 @@ namespace InstaIssue.CallCenter.UILayer
             this.lblCContract = new System.Windows.Forms.Label();
             this.lblCName = new System.Windows.Forms.Label();
             this.lblCID = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pnlReviews = new System.Windows.Forms.Panel();
-            this.pnlIssues = new System.Windows.Forms.Panel();
-            this.pnlBlank = new System.Windows.Forms.Panel();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.dgvData = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
             this.panel2.SuspendLayout();
-            this.pnlInformation.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.btnDashboard);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.panel4);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -89,42 +79,6 @@ namespace InstaIssue.CallCenter.UILayer
             this.btnDashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnDashboard.UseVisualStyleBackColor = true;
             this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
-            // 
-            // button2
-            // 
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(28)))), ((int)(((byte)(51)))));
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(50)))), ((int)(((byte)(55)))));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.button2.Image = global::InstaIssue.Properties.Resources.Review;
-            this.button2.Location = new System.Drawing.Point(0, 203);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(159, 82);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Reviews";
-            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button1
-            // 
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(28)))), ((int)(((byte)(51)))));
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(50)))), ((int)(((byte)(55)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.button1.Image = global::InstaIssue.Properties.Resources.Issues;
-            this.button1.Location = new System.Drawing.Point(0, 291);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 82);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Issues";
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // panel4
             // 
@@ -160,48 +114,8 @@ namespace InstaIssue.CallCenter.UILayer
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(159, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1140, 81);
+            this.panel2.Size = new System.Drawing.Size(764, 81);
             this.panel2.TabIndex = 1;
-            // 
-            // pnlInformation
-            // 
-            this.pnlInformation.Controls.Add(this.label1);
-            this.pnlInformation.Controls.Add(this.dataGridView1);
-            this.pnlInformation.Location = new System.Drawing.Point(342, 81);
-            this.pnlInformation.Name = "pnlInformation";
-            this.pnlInformation.Size = new System.Drawing.Size(583, 430);
-            this.pnlInformation.TabIndex = 2;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(10, 34);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(562, 385);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // lblDate
-            // 
-            this.lblDate.AutoSize = true;
-            this.lblDate.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.lblDate.Location = new System.Drawing.Point(635, 9);
-            this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(120, 24);
-            this.lblDate.TabIndex = 6;
-            this.lblDate.Text = "2021-04-17";
-            // 
-            // lblTime
-            // 
-            this.lblTime.AutoSize = true;
-            this.lblTime.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.lblTime.Location = new System.Drawing.Point(635, 44);
-            this.lblTime.Name = "lblTime";
-            this.lblTime.Size = new System.Drawing.Size(63, 24);
-            this.lblTime.TabIndex = 5;
-            this.lblTime.Text = "14:23";
             // 
             // pnlSeperator
             // 
@@ -278,48 +192,44 @@ namespace InstaIssue.CallCenter.UILayer
             this.lblCID.TabIndex = 13;
             this.lblCID.Text = "Client ID";
             // 
-            // label1
+            // lblDate
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.label1.Location = new System.Drawing.Point(10, 10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 21);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "Information: ...";
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblDate.Location = new System.Drawing.Point(635, 9);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(120, 24);
+            this.lblDate.TabIndex = 6;
+            this.lblDate.Text = "2021-04-17";
             // 
-            // pnlReviews
+            // lblTime
             // 
-            this.pnlReviews.Location = new System.Drawing.Point(167, 89);
-            this.pnlReviews.Name = "pnlReviews";
-            this.pnlReviews.Size = new System.Drawing.Size(169, 410);
-            this.pnlReviews.TabIndex = 3;
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblTime.Location = new System.Drawing.Point(635, 44);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(63, 24);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "14:23";
             // 
-            // pnlIssues
+            // dgvData
             // 
-            this.pnlIssues.Location = new System.Drawing.Point(931, 89);
-            this.pnlIssues.Name = "pnlIssues";
-            this.pnlIssues.Size = new System.Drawing.Size(169, 410);
-            this.pnlIssues.TabIndex = 4;
-            // 
-            // pnlBlank
-            // 
-            this.pnlBlank.Location = new System.Drawing.Point(1118, 91);
-            this.pnlBlank.Name = "pnlBlank";
-            this.pnlBlank.Size = new System.Drawing.Size(169, 410);
-            this.pnlBlank.TabIndex = 5;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(167, 90);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowTemplate.Height = 25;
+            this.dgvData.Size = new System.Drawing.Size(749, 413);
+            this.dgvData.TabIndex = 2;
             // 
             // Information
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
-            this.ClientSize = new System.Drawing.Size(1299, 512);
-            this.Controls.Add(this.pnlBlank);
-            this.Controls.Add(this.pnlIssues);
-            this.Controls.Add(this.pnlReviews);
-            this.Controls.Add(this.pnlInformation);
+            this.ClientSize = new System.Drawing.Size(923, 512);
+            this.Controls.Add(this.dgvData);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -334,9 +244,7 @@ namespace InstaIssue.CallCenter.UILayer
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.pnlInformation.ResumeLayout(false);
-            this.pnlInformation.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -347,11 +255,7 @@ namespace InstaIssue.CallCenter.UILayer
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.PictureBox imgLogo;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnDashboard;
-        private System.Windows.Forms.Panel pnlInformation;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label lblDate;
         private System.Windows.Forms.Label lblTime;
         private System.Windows.Forms.Panel pnlSeperator;
@@ -361,9 +265,6 @@ namespace InstaIssue.CallCenter.UILayer
         private System.Windows.Forms.Label lblCContract;
         private System.Windows.Forms.Label lblCName;
         private System.Windows.Forms.Label lblCID;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel pnlReviews;
-        private System.Windows.Forms.Panel pnlIssues;
-        private System.Windows.Forms.Panel pnlBlank;
+        private System.Windows.Forms.DataGridView dgvData;
     }
 }
