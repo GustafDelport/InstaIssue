82da947fb8e040a5ce8ace1a6dab50ca2b9f61e1
JJ <jj@oxibux.com>
2021-04-24 10:50:46 +0200

    Database
    Add database connection class
    Add database Class Library

---
+++ Database/Connection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Database
+{
+    public class Connection
+    {
+        public SqlConnection database;
+
+        private string
+            SQLConnection =
+                "Server=.; Initial Catalog= instaissue; Integrated Security=SSPI"; // Server=localhost\\SQLEXPRESS;Initial Catalog=instaissue;Integrated Security=False;";
+
+        public void Connect()
+        {
+            try
+            {
+                database = new SqlConnection(SQLConnection); // To call this database connection: Connection.database.Open(); 
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e);
+                throw;
+            }
+            
+        }
+
+        public SqlCommand RunCommand(string command)
+        {
+            SqlCommand cmd = new SqlCommand(command, database);
+            return cmd;
+        } 
+    }
+}
---
+++ Database/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    class Data
+    {
+        private Connection connection = new Connection();
+
+        public Data()
+        {
+            connection.Connect();
+        }
+
+        // public Boolean UpdateRole(int userID, int roleID)
+        // {
+        //
+        // }
+    }
+}
