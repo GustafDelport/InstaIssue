0e17ede2372ab1d1edeeb07b6aeadcef1066294d
Gustaf Delport <gustafdelport@gmail.com>
2021-04-26 11:32:41 +0200

    Created and designed AdminCenter

--- InstaIssue/Globals.cs
+++ InstaIssue/Globals.cs
@@ -32,7 +32,13 @@ namespace InstaIssue
         public static StaffDeleteForm staffDeleteForm;
         public static StaffEditForm staffEditForm;
         public static StaffViewForm staffViewForm;
+
         //==========================================================================
+
+        //Tracking
+        //==========================================================================
+        public static String StaffID;
+        public static String nationalID;
 
     }
 }
