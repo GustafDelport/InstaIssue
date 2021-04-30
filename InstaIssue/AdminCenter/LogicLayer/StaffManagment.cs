99411e9913bd61ea4774283b8c7fb74b8fca0ca8
Conradt <conradtvh@gmail.com>
2021-04-30 10:07:06 +0200

    DeleteStaff

--- InstaIssue/AdminCenter/LogicLayer/StaffManagment.cs
+++ InstaIssue/AdminCenter/LogicLayer/StaffManagment.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Database;
 
 namespace InstaIssue.AdminCenter.LogicLayer
 {
@@ -35,7 +36,10 @@ namespace InstaIssue.AdminCenter.LogicLayer
 
         public Boolean DeleteStaff(String staffID)
         {
-            //All Fields later;
+            //Calls method from Database.data
+            Data data = new Data();
+            //Get data from form
+            data.DeleteEntry(id, tblName, idCol);
             return false;
         }
     }
